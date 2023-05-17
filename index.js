b06791d6f918b771f7b6222a8f9c075a6cd5e32a
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-05-17 16:13:02 +0800

    adding example crud routes

--- index.js
+++ index.js
@@ -13,6 +13,31 @@ app.get("/fruit", (request, response) => {
   response.send(fruit);
 });
 
+// http://localhost:8080/fruit - Testing request.body
+app.post("/fruit", (req, res) => {
+  let fruitToAdd = req.body.fruit;
+  fruit.push(fruitToAdd);
+  res.send(fruit);
+});
+
+// http://localhost:8080/fruit - Testing request.body && request.params
+app.put("/fruit/:name", (req, res) => {
+  let fruitToAdd = req.body.fruit;
+  let fruitToReplace = req.params.name;
+  let index = fruit.indexOf(fruitToReplace);
+  fruit.splice(index, 1, fruitToAdd);
+  res.send(fruit);
+});
+
+// http://localhost:8080/fruit - Testing request.params
+app.delete("/fruit/:name", (req, res) => {
+  let fruitToReplace = req.params.name;
+  let index = fruit.indexOf(fruitToReplace);
+  fruit.splice(index, 1);
+  res.send(fruit);
+});
+
+// http://localhost:8080/user/Bill - Testing request Parameters
 app.get("/user/:name", (req, res) => {
   res.send(`Hello ${req.params.name}`);
 });
