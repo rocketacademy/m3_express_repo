e3469e3362442d39b334f1b67795184f562879f3
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-05-17 17:11:25 +0800

    express controllers and routers

--- index.js
+++ index.js
@@ -3,47 +3,21 @@ const app = express();
 
 const fruit = ["Apple", "Banana"];
 
+const FruitController = require("./Controllers/FruitController");
+const FruitRouter = require("./Routers/FruitRouter");
+
+// setup fruitController instance and fruitRouter instance
+const fruitController = new FruitController();
+const fruitRouter = new FruitRouter(fruitController, express);
+
 // Setting up middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// http://localhost:8080/
-app.get("/", (request, response) => {
-  response.send("Hello World");
-});
+app.use("/fruit", fruitRouter.route());
 
-// http://localhost:8080/fruit
-app.get("/fruit", (request, response) => {
-  response.send(fruit);
-});
-
-// http://localhost:8080/fruit - Testing request.body
-app.post("/fruit", (req, res) => {
-  let fruitToAdd = req.body.fruit;
-  fruit.push(fruitToAdd);
-  res.send(fruit);
-});
-
-// http://localhost:8080/fruit - Testing request.body && request.params
-app.put("/fruit/:name", (req, res) => {
-  let fruitToAdd = req.body.fruit;
-  let fruitToReplace = req.params.name;
-  let index = fruit.indexOf(fruitToReplace);
-  fruit.splice(index, 1, fruitToAdd);
-  res.send(fruit);
-});
-
-// http://localhost:8080/fruit - Testing request.params
-app.delete("/fruit/:name", (req, res) => {
-  let fruitToReplace = req.params.name;
-  let index = fruit.indexOf(fruitToReplace);
-  fruit.splice(index, 1);
-  res.send(fruit);
-});
-
-// http://localhost:8080/user/Bill - Testing request Parameters
-app.get("/user/:name", (req, res) => {
-  res.send(`Hello ${req.params.name}`);
+app.use("/", (req, res) => {
+  res.send("Incorrect path");
 });
 
 app.listen(8080, () => {
